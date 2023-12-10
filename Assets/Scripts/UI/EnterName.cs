38150e5efb1f2c532c789055e3945fc018f66efe
Aleksandra <118261243+inoidea@users.noreply.github.com>
2023-12-10 17:08:26 +0300

    task5

---
+++ Assets/Scripts/UI/EnterName.cs
@@ -0,0 +1,22 @@
+﻿using Main;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class EnterName : MonoBehaviour
+{
+    [SerializeField] TMP_InputField chatInput;
+    [SerializeField] Button enterBtn;
+
+    private void Awake()
+    {
+        enterBtn.onClick.AddListener(() => AddUser(chatInput.text));
+    }
+
+    private void AddUser(string userName)
+    {
+        gameObject.SetActive(false);
+    }
+}
